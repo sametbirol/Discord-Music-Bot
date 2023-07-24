30c77c271232d66e45bf3b6d4a7a36fc65b53768
sametbirol <sametbirol34@gmail.com>
2023-07-24 22:04:37 +0300

    logging and filtering

--- logging_result.py
+++ logging_result.py
@@ -1,4 +1,5 @@
 import os
+from error_logger import log_error
 from dotenv import load_dotenv
 load_dotenv()
 TOP_LIMIT = int(os.getenv("TOP_LIMIT"))
@@ -10,22 +11,40 @@ def log_music_search_result(server_name, search_query, result):
     collectiveList = []
     with open(filename, "a", encoding="utf-8") as file:
         file.write(f"Search Query: {search_query}\n")
-        for entry in result["entries"]:
-            filtered = filter_formats(entry)
-            # print("filtered data", filtered)
-            sorted_data = sorted(filtered, key=lambda x: x['abr'])
-            # print("aorted data", sorted_data)
-            collectiveList.append(sorted_data[0])
-            # file.write(f'{sorted_data[0]} \n')
-            # file.write(f'{sorted_data} \n')
-            allfiltered_sorted = sorted(filter_formats(entry), key=lambda x: x['abr'])
-            allfiltered_sorted = sorted(filter_formats(entry), key=lambda x: x['abr'])
-            file.write(f'{allfiltered_sorted} \n')
+        try:
+            for entry in result["entries"]:
+                lowest_kpbs_format = filter_formats(entry)
+                if lowest_kpbs_format != False:
+                    collectiveList.append(lowest_kpbs_format)
+                    file.write(f'{lowest_kpbs_format} \n')
+        except Exception as e:
+            log_error(e)
     return collectiveList
 
 
 def filter_formats(entry):
-    
-    filtered_formats = [{'source': fmt['url'], 'title': entry['title'], 'abr' : fmt['abr']} for fmt in entry['formats'] if fmt.get('abr', 0) and BOTTOM_LIMIT <= fmt['abr'] <= TOP_LIMIT and fmt['acodec'] != 'none' and fmt['vcodec'] == 'none']
-    return filtered_formats
-    
+    copy_top_limit = TOP_LIMIT
+    filtered_formats = []
+    try:
+        while not filtered_formats and copy_top_limit < 256:
+            filtered_formats = [
+                {
+                    "source": fmt["url"],
+                    "title": entry["title"],
+                    "abr": fmt["abr"],
+                }
+                for fmt in entry["formats"]
+                if fmt.get("abr", 0)
+                and BOTTOM_LIMIT <= fmt["abr"] <= copy_top_limit
+                and fmt["acodec"] != "none"
+                and fmt["vcodec"] == "none"
+            ]
+            copy_top_limit += 16
+
+        if filtered_formats:
+            return sorted(filtered_formats, key=lambda x: x["abr"])[0]
+        else:
+            return False
+    except Exception as e:
+        log_error(e)
+        return False
