9d2b8a1f4c5530c083afc459ad07a26f77961206
sametbirol <sametbirol34@gmail.com>
2023-07-24 22:04:19 +0300

    remove whitespace

--- music_bot.py
+++ music_bot.py
@@ -2,27 +2,23 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-
+from admin_cog import admin_cog
 from help_cog import help_cog
 from music_cog import music_cog
 
 load_dotenv()
+
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.all()
 
-bot = commands.AutoShardedBot(command_prefix="!",intents=intents)
-
-
+bot = commands.Bot(command_prefix="!",intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} connected to Discord!')
     bot.remove_command("help")
     await bot.add_cog(help_cog(bot))
     await bot.add_cog(music_cog(bot))
-    print(f'Configurations connecteddd to Discord!!')
-
-
-
+    await bot.add_cog(admin_cog(bot))
 bot.run(TOKEN)
